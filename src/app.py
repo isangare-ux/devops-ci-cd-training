8b410766517f297631687e8a62000fd6b1c877f9
root <root@PC-Sangare.localdomain>
2025-12-05 13:57:03 +0100

    Add Postgres service and health check

--- src/app.py
+++ src/app.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 import os
+import sqlalchemy
+from sqlalchemy import text
 import redis
+
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "fastapi_db")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "fastapi_user")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "secret123")
+
+# PostgreSQL Engine
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+engine = sqlalchemy.create_engine(DATABASE_URL, echo=False, future=True)
+
 
 app = FastAPI()
 
@@ -17,10 +30,23 @@ def add(a: int, b: int) -> int:
 
 @app.get("/health")
 def health():
+    # Redis Check
     try:
         r.ping()
         redis_status = "ok"
     except Exception:
         redis_status = "unreachable"
 
-    return {"status": "ok", "redis": redis_status}
+    # Postgres Check
+    try:
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+        db_status = "ok"
+    except Exception:
+        db_status = "unreachable"
+
+    return {
+        "status": "ok",
+        "redis": redis_status,
+        "postgres": db_status
+    }
